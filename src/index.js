54c4bf318aace70a874f90688dec700cc0bcf99e
Matheus Aurélio <matheusaurelio2004@gmail.com>
2023-04-30 16:16:47 -0300

    :recycle: refactor: Create folder to middlewares

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ require('dotenv').config();
 require('express-async-errors');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(express.json());
 
+const cors = require('./app/middlewares/cors');
 const routes = require('./routes');
+const errorHandler = require('./app/middlewares/errorHandler');
 
+app.use(cors);
 app.use(routes);
-app.use((error, req, res, next) => {
-  console.log('Error Handler -', error);
-  res.sendStatus(500);
-});
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
