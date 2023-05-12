8dd7e11275375d2513a8d3850942108f3f100545
Matheus Aurélio <matheusaurelio2004@gmail.com>
2023-05-12 15:14:50 -0300

    Remove origin localhost

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000, https://mycontacts-frontend-matheus-aurelio.vercel.app');
+  res.setHeader('Access-Control-Allow-Origin', 'https://mycontacts-frontend-matheus-aurelio.vercel.app');
   res.setHeader('Access-Control-Allow-Headers', '*')
   res.setHeader('Access-Control-Allow-Methods', '*');
   res.setHeader('Access-Control-Max-Age', '120');
